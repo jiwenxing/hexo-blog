c0e0ac6238e6895fb533bf9373fbab85b4475732
jiwenxing <jiwenxing@jd.com>
2017-05-14 19:53:51 +0800

    add recent comments widget

---
+++ themes/maupassant/source/js/gentie.js
@@ -0,0 +1,29 @@
+var Comments = (function(){
+
+	var getComments = function(url){
+		console.log("hello begin!");
+		fetch(url).then(function(res) {
+		  // res instanceof Response == true.
+		  if (res.ok) {
+		    res.json().then(function(data) {
+		      console.log(data.content);
+		      $(".ds-recent-comments").html(data.content);
+		    });
+		  } else {
+		    console.log("Looks like the response wasn't perfect, got status", res.status);
+		  }
+		}, function(e) {
+		  console.log("Fetch failed!", e);
+		});
+	}
+
+	return {
+		init:function(){
+            var USER_NAME = "jverson";
+			getComments("http://gentie.jverson.com/getComments?user="+USER_NAME);
+		}
+	}
+})();
+$(function(){
+	Comments.init();
+})
